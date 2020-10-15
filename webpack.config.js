1ba65b17a10d505683d2d0cb7188bd27453d93be
NookieNoo <biv0208@yandex.ru>
2020-10-15 21:45:00 +0300

    Заново настраиваем webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,45 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
-
-const isDev = process.env.NODE_ENV === 'development';
-
-const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+const { join } = require('path');
+const outputPath = join(process.cwd(), '/dist');
+const options = { 
+    port: 22001,
+    static: outputPath,
+    progress: 'minimal',
+    liveReload: true,
+    status: true,
+ };
 
 module.exports = {
-    context: path.resolve(__dirname, "assets"),
-    mode: 'development',
-    entry: {
-        main: ['@babel/polyfill', './index.jsx'],
-    },
-    output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'public/dist'),
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@models': path.resolve(__dirname, 'assets/models'),
-            "@": path.resolve(__dirname, "assets"),
-        },
-    },
-    optimization: {
-        splitChunks: {
-            chunks: "all"
-        }
-    },
-    devServer: {
-        port: 4200,
-        hot: isDev,
-    },
-    devtool: isDev ? 'source-map' : '',
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: "./index.html",
-            minify: {
-                collapseWhitespace: !isDev,
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new BundleAnalyzerPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loader: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-react'
-                        ]
-                    }
-                }
-            },
-        ]
-    }
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[contenthash].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new Serve(options),
+    new CleanWebpackPlugin(),
+    
+  ],
+  watch: true,
+  mode: 'development'
 };
