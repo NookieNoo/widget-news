97e63f0e05a454bfb3da0e51da55cac6a2a8c6a4
NookieNoo <biv0208@yandex.ru>
2020-11-23 23:24:06 +0300

    Начинаем переходить на mui

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React from 'react';
 import Header from '@app-universal/Header';
 import Footer from '@app-universal/Footer';
 import RecordsList from '@app-pages/index/RecordsList';
 
-import '@styles/bootstrap.min.css';
-import '@styles/style.css';
+import { Container, AppBar, Toolbar, IconButton, Typography, Button, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(1), //по умоллчанию 8px
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function App(props) {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Header />
-            <RecordsList />
-            <Footer
-                isLogged={false}
-            />
-        </div>
+        <AppBar position="fixed">
+            <Container fixed>
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title}>Widget News</Typography>
+                    <Box mr={3}>
+                        <Button color="inherit" variant="outlined">
+                            Log in
+                        </Button>
+                    </Box>
+                    <Button color="secondary" variant="contained">
+                        Sign Up
+                    </Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
