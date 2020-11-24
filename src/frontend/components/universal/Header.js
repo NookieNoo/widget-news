c0381896482f64c78017e7c543e351a6bfd1a205
NookieNoo <biv0208@yandex.ru>
2020-11-24 22:08:29 +0300

    Добавлена менюшка слева, в карточках теперь реальные данные

--- src/frontend/components/universal/Header.js
+++ src/frontend/components/universal/Header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from '@images/logo.jpg';
-
 import {
     Container,
     AppBar,
@@ -15,11 +13,17 @@ import {
     DialogContentText,
     DialogActions,
     TextField,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
 } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-
-// import TextField from '@material-ui/core/TextField';
+import Panel from './Panel';
 
 const useStyles = makeStyles((theme) => ({
     menuButton: {
@@ -27,6 +31,9 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
         flexGrow: 1,
+    },
+    list: {
+        width: 250,
     },
 }));
 
@@ -42,63 +49,99 @@ function Header(props) {
         setOpen(false);
     };
 
+    const [state, setState] = React.useState(false);
+    const toggleDrawer = (open) => {
+        setState(open);
+    };
+
     return (
-        <AppBar position="fixed">
-            <Container fixed>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        className={classes.menuButton}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography className={classes.title}>Widget News</Typography>
-                    <Box mr={3}>
-                        <Button color="inherit" variant="outlined" onClick={handleClickOpen}>
-                            Log in
-                        </Button>
-                        <Dialog
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="form-dialog-title"
+        <>
+            <AppBar position="fixed">
+                <Container fixed>
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            className={classes.menuButton}
+                            onClick={() => toggleDrawer(true)}
                         >
-                            <DialogTitle id="form-dialog-title">Log in</DialogTitle>
-                            <DialogContent>
-                                <DialogContentText>Log in to see videos</DialogContentText>
-                                <TextField
-                                    autoFocus
-                                    margin="dense"
-                                    id="name"
-                                    label="Email"
-                                    type="email"
-                                    fullWidth
-                                />
-                                <TextField
-                                    margin="dense"
-                                    id="pass"
-                                    label="Password"
-                                    type="password"
-                                    fullWidth
-                                />
-                            </DialogContent>
-                            <DialogActions>
-                                <Button onClick={handleClose} color="primary">
-                                    Cancel
-                                </Button>
-                                <Button onClick={handleClose} color="primary">
-                                    Log in
-                                </Button>
-                            </DialogActions>
-                        </Dialog>
-                    </Box>
-                    <Button color="secondary" variant="contained">
-                        Sign Up
-                    </Button>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                            <MenuIcon />
+                        </IconButton>
+
+                        <nav className={classes.drawer}>
+                            <Drawer
+                                open={state}
+                                onClose={() => toggleDrawer(false)}
+                                variant="temporary"
+                            >
+                                <div className={classes.list}>
+                                    <List>
+                                        {['Статьи', 'Категории', 'Подкатегории', 'Еще'].map(
+                                            (text, index) => (
+                                                <ListItem button key={text}>
+                                                    <ListItemIcon>
+                                                        {index % 2 === 0 ? (
+                                                            <InboxIcon />
+                                                        ) : (
+                                                            <MailIcon />
+                                                        )}
+                                                    </ListItemIcon>
+                                                    <ListItemText primary={text} />
+                                                </ListItem>
+                                            )
+                                        )}
+                                    </List>
+                                </div>
+                            </Drawer>
+                        </nav>
+
+                        <Typography className={classes.title}>Widget News</Typography>
+                        <Box mr={3}>
+                            <Button color="inherit" variant="outlined" onClick={handleClickOpen}>
+                                Log in
+                            </Button>
+                            <Dialog
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="form-dialog-title"
+                            >
+                                <DialogTitle id="form-dialog-title">Log in</DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText>Log in to see videos</DialogContentText>
+                                    <TextField
+                                        autoFocus
+                                        margin="dense"
+                                        id="name"
+                                        label="Email"
+                                        type="email"
+                                        fullWidth
+                                    />
+                                    <TextField
+                                        margin="dense"
+                                        id="pass"
+                                        label="Password"
+                                        type="password"
+                                        fullWidth
+                                    />
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose} color="primary">
+                                        Cancel
+                                    </Button>
+                                    <Button onClick={handleClose} color="primary">
+                                        Log in
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+                        </Box>
+                        <Button color="secondary" variant="contained">
+                            Sign Up
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
     );
 }
 
