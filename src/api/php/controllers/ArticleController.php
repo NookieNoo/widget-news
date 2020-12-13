1f0883ccb5a7f712f616ec4b1d76c51a65043cb3
iamanatolich <alex-goncharov@inbox.ru>
2020-12-13 16:51:48 +0300

    yii basic

---
+++ src/api/php/controllers/ArticleController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\models\Articles;
+use yii\rest\ActiveController;
+
+class ArticleController extends ActiveController
+{
+    public $enableCsrfValidation = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+            'cors' => [
+                // restrict access to
+                'Origin' => ['*'],
+                // Allow  methods
+                'Access-Control-Request-Method' => ['POST', 'PUT', 'OPTIONS', 'GET'],
+                // Allow only headers 'X-Wsse'
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Headers' => ['Content-Type'],
+                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                //'Access-Control-Allow-Credentials' => true,
+                // Allow OPTIONS caching
+                'Access-Control-Max-Age' => 3600,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                'Access-Control-Expose-Headers' => ['*'],
+            ],
+        ];
+        return $behaviors;
+    }
+
+
+    public $modelClass = Articles::class;
+}
---
+++ src/api/php/models/Categories.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ *
+ * @property Articles[] $articles
+ * @property Subcategory[] $subcategories
+ */
+class Categories extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'description'], 'required'],
+            [['description'], 'string'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+        ];
+    }
+
+    public function fields()
+    {
+        return [
+            'id',
+            'name',
+            'description',
+        ];
+    }
+
+    /**
+     * Gets query for [[Articles]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticles()
+    {
+        return $this->hasMany(Articles::className(), ['categoryId' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Subcategories]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSubcategories()
+    {
+        return $this->hasMany(Subcategory::className(), ['categoryId' => 'id']);
+    }
+}
